991cd14b414496734f9d61fb7013aff46cd432dc
hbonnavaud <hbonnavaud@gmail.com>
2024-08-25 22:25:08 +0200

    work in progress

---
+++ navigation_envs/__init__.py
@@ -0,0 +1,38 @@
+from .environment import Environment
+from .goal_conditioned_environment import GoalConditionedEnvironment
+import importlib
+from .. import settings
+
+
+# For every environment module, we use try except statements in case one environment have unmeet dependencies but the
+# user wants to use the others.
+# For an example, it allows you to import GridWorld without having mujoco or vizdoom installed.
+
+# Import grid-world
+try:
+    from .grid_world import *
+except Exception as e:
+    if not settings.supress_import_warnings:
+        print(f"Warning: module 'grid_world' cannot be imported due to the following error: ", e, sep="")
+
+
+# Import point-maze
+try:
+    from .point_maze import *
+except Exception as e:
+    if not settings.supress_import_warnings:
+        print(f"Warning: module 'point_maze' cannot be imported due to the following error: ", e, sep="")
+
+# Import ant-maze
+try:
+    from .ant_maze import *
+except Exception as e:
+    if not settings.supress_import_warnings:
+        print(f"Warning: module 'ant_maze' cannot be imported due to the following error: ", e, sep="")
+
+# Import doom-maze
+try:
+    from .doom_maze import *
+except Exception as e:
+    if not settings.supress_import_warnings:
+        print(f"Warning: module 'doom_maze' cannot be imported due to the following error: ", e, sep="")
