68c15a3b0c78f4b7dcdd3c3578ef36d524ee9fdd
hedwin <hbonnavaud@gmail.com>
2025-06-05 14:55:00 +0200

    repaired imports

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rlnav",
-    version="1.4.1",
+    version="1.4.2",
     description="Reinforcement Learning Navigation Environments for Gym and Gymnasium",
     author="Hedwin Bonnavaud",
     author_email="hbonnavaud@gmail.com",
